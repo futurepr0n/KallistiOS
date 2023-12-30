51ec236450bf94076ebceb3c369e288e4a39efd5
QuzarDC <quzar@completext.com>
2023-12-30 14:26:29 -0500

    Clean up example and add it to the video folder makefile

--- examples/dreamcast/video/multibuffer/multibuffer.c
+++ examples/dreamcast/video/multibuffer/multibuffer.c
@@ -26,11 +26,11 @@ int main(int argc, char **argv) {
 
     /* Set the video mode */
     vid_set_mode(DM_640x480 | DM_MULTIBUFFER, PM_RGB565);
-    
+
     /* Cycle through each frame buffer populating it with different 
         patterns and text labelling it. */
     for(mb = 0; mb < vid_mode->fb_count; mb++) {
-        
+
         for(y = 0; y < 480; y++)
             for(x = 0; x < 640; x++) {
                 int c = (x ^ y) & 0xff;
@@ -41,14 +41,16 @@ int main(int argc, char **argv) {
 
         snprintf(text_buff, 20, "This is FB %u", (mb + 1) % vid_mode->fb_count);
         bfont_draw_str(vram_s + (640 * BFONT_HEIGHT) + (BFONT_THIN_WIDTH * 2), 640, 1, text_buff);
-            
+
+        /* This tells the pvr to move to the framebuffer we've been drawing to, 
+            then adjusts the vram_* pointers to the next one. */
         vid_flip(-1);
     }
 
     printf("\n\nPress all buttons simultaneously to exit.\n");
     fflush(stdout);
 
-    /* Now flip through each frame until stopped */
+    /* Now flip through each frame until stopped, waiting a bit each time. */
     while(1) {
         vid_flip(-1);
         timer_spin_sleep(1500);
