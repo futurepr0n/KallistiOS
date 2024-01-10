7bda81c59ae5317f74f5fe5274de60788e06021d
Falco Girgis <gyrovorbis@gmail.com>
2024-01-09 20:19:04 -0600

    Fixed getpid() and implemented kill()
    - Our implementation of getpid() for newlib was sketchily returning the
      current thread ID as the process ID
    - We're now using a signle constnant value, KOS_PID, as the single
      process ID for a KOS executable, which is returned by getpid()
    - kill() is now implemented to kill the current process if the correct
      PID is passed or a negative value which is meant to kill a related
    group

--- kernel/libc/newlib/newlib_kill.c
+++ kernel/libc/newlib/newlib_kill.c
@@ -1,16 +1,27 @@
 /* KallistiOS ##version##
 
    newlib_kill.c
-   Copyright (C)2004 Megan Potter
+   Copyright (C) 2004 Megan Potter
+   Copyright (C) 2024 Falco Girgis
 
 */
 
+#include <kos/thread.h>
 #include <sys/reent.h>
 #include <errno.h>
 
-int _kill_r(struct _reent * reent, int pid, int sig) {
-    (void)pid;
-    (void)sig;
-    reent->_errno = EINVAL;
+int _kill_r(struct _reent *reent, int pid, int sig) {
+    if(pid <= 0 || pid == KOS_PID) {
+        /* NULL signals are simply ignored, while any other
+           signal will terminate the application with the signal
+           value as its return value.
+         */
+        if(sig)
+            exit(sig);
+
+        return 0;
+    }
+
+    reent->_errno = ESRCH;
     return -1;
 }
